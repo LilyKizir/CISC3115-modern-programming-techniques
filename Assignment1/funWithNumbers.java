0d8eed5b26dd05ced71911560e09a5ee10e62290
Lily Kiziriya <107429746+LilyKizir@users.noreply.github.com>
2025-08-14 14:07:19 -0400

    assignment 1 complete
    -created readme.md
    -created funWithNumbers.java

---
+++ Assignment1/funWithNumbers.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+public class funWithNumbers {
+    public static void main(String[] args){
+        int num1, num2, num3;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter 3 numbers, separated by a space");
+        num1 = input.nextInt();
+        num2 = input.nextInt();
+        num3 = input.nextInt();
+        System.out.println("Results:");
+        System.out.println("allAreEqual: " + allAreEqual(num1,num2,num3));
+        System.out.println("twoAreEqual: " + twoAreEqual(num1,num2,num3));
+        System.out.println("noneAreEqual: " + noneAreEqual(num1,num2,num3));
+        System.out.println("areAscending: " + areAscending(num1,num2,num3));
+        System.out.println("areDescending: " + areDescending(num1,num2,num3));
+        System.out.println("strictlyAscending: " + strictlyAscending(num1,num2,num3));
+        System.out.println("strictlyDescending: " + strictlyDescending(num1,num2,num3));
+
+    }
+
+    public static boolean allAreEqual(int a, int b, int c){
+        return a == b && b == c && c == a;
+    }
+    public static boolean twoAreEqual(int a, int b, int c){
+        return !( a == b && b == c && c == a) && !( a != b && b != c && c != a );
+    }
+    public static boolean noneAreEqual(int a, int b, int c){
+        return a != b && b != c && c != a;
+    }
+    public static boolean areAscending(int a, int b, int c){
+        return a <= b && b <= c;
+    }
+    public static boolean areDescending(int a, int b, int c){
+        return a >= b && b >= c;
+    }
+    public static boolean strictlyAscending(int a, int b, int c){
+        return a < b && b < c;
+    }
+    public static boolean strictlyDescending(int a, int b, int c){
+        return a > b && b > c;
+    }
+}
---
+++ Assignment1/readme.md
@@ -0,0 +1,39 @@
+# Assignment 1
+Write a program that reads three numbers from keyboard, and then prints out some information about the relationships between the numbers - whether they are all equal, in ascending order, in descending order, and so on.
+## Task
+To determine these relationships, write the following boolean-valued functions:
+```java
+boolean allAreEqual(int a, int b, int c);
+boolean twoAreEqual(int a, int b, int c); // false if all three are equal
+boolean noneAreEqual(int a, int b, int c);
+boolean areAscending(int a, int b, int c); // true if a <= b <= c
+boolean areDescending(int a, int b, int c); // true if a >= b >= c
+boolean strictlyAscending(int a, int b, int c); // true if a < b < c
+boolean strictlyDescending(int a, int b, int c); // true if a > b > c
+```
+Try to "talk boolean" — i.e., use boolean variables and conditional operators rather than if statements where possible
+For example, write:
+```java
+boolean isNegative = x < 0;
+```
+rather than:
+```java
+boolean isNegative;
+if (x i< 0)
+isNegtive = true;
+else
+isNegative = false;
+```
+Below is how the output should look:
+```bash
+first number? 1
+second number? 2
+third number? 3
+allAreEqual: false
+twoAreEqual: false
+noneAreEqual: true
+areAscending: true
+areDescending: false
+strictlyAscending: true
+strictlyDescending: false
+```
